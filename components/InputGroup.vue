74936509cfc64235ca3ebd31b318c2514d934254
Michael Brecht <brecht.michael.b@gmail.com>
2021-02-07 12:54:13 -0700

    adds styling

--- components/InputGroup.vue
+++ components/InputGroup.vue
@@ -6,3 +6,17 @@
     <Button content="Create Grid" />
   </div>
 </template>
+
+<style scoped>
+.input-group {
+  display: flex;
+  flex-direction: column;
+  row-gap: 1rem;
+  align-items: center;
+}
+
+.name {
+  font-size: 3rem;
+  font-weight: normal;
+}
+</style>
